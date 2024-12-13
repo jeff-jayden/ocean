97cecfeac101e7625b504c5cd3f88d58d30189a0
jeff-jayden <jeff-jayden@users.noreply.github.com>
2024-12-13 21:01:13 +0800

    feat: test_commitlint

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import './App.scss';
 
 import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
 import App from './App.vue';
 import router from './router';
+import 'element-plus/dist/index.css';
 
 const app = createApp(App);
 
+app.use(ElementPlus);
 app.use(router);
 
 app.mount('#app');
